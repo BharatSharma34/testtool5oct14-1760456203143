709fdc74fa083773fb281b35116c09b6736f11f2
BharatSharma34 <bharatsharma@ducktaleit.com>
2025-10-14 21:06:45 +0530

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,31 @@
+"""
+testTool5oct14 - Custom Lambda Tool
+Description: test
+
+IMPORTANT: Only edit the code in the main() function below.
+The Lambda handler will be automatically appended during deployment.
+DO NOT add lambda_handler code here - it will be added automatically.
+"""
+
+def main(event_body):
+    """
+    Main function for testTool5oct14
+    This function will be called by the Lambda handler.
+    
+    Parameters:
+    event_body: dict - The request body passed to the Lambda function
+    
+    Returns:
+    dict - JSON-serializable response
+    """
+    # Your tool logic here
+    # Access parameters from event_body
+    # Example: user_input = event_body.get('input_data', '')
+    
+    return {
+        "success": True,
+        "message": "Hello from testTool5oct14!",
+        "data": event_body
+    }
+
+# You can add helper functions below
